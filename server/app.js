88141c2d0f39250477d195f584d8d06e57e713f0
David Chou <davidchou2009@gmail.com>
2022-06-24 19:57:56 -0700

    update git ignore

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+const routes = require('/routes')
 
 require('dotenv').config()
 
 //access-control-allow-origin
 const cors = require('cors')
 app.use(cors())
+app.use(routes)
 
 //resolving req data
 app.use(express.urlencoded({extended:false}))
