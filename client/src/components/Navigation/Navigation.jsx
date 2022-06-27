0e1a56e778ef73abb4b791a6f4060469fe880b0d
David Chou <davidchou2009@gmail.com>
2022-06-27 15:00:28 -0700

    add login front end

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -10,6 +10,7 @@ import TopNavigation from "./TopNav/TopNavigation";
 import styled from "styled-components";
 import RegistrationToken from '../Hire/RegistrationToken';
 import Registration from '../Authentication/Registration';
+import Login from '../Authentication/Login';
 
 
 const Main = styled.main`
@@ -91,6 +92,7 @@ export default function Sidebar(props) {
             <div className="main-content-container">
               <Route path="/hire/register"  component={props => <RegistrationToken />} />
               <Route path={`/register${path}`} component={props => <Registration />} />
+              <Route path={'/login'} component={props => <Login />} />
             </div>
           </Main>
         </React.Fragment>
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const RegisterRouter = require("./registerRouter");
+const apiRoutes = require('./api');
+const homeRoutes = require('/homeRoutes');
 
-router.use('/api', RegisterRouter);
-
+router.use('/api', apiRoutes);
+router.use('/',homeRoutes);
 module.exports = router;
 
