90480c79169f21bf612e9f06da59e789df4edf15
Winston Gao <lgao9@ucsc.edu>
2022-06-28 13:24:17 -0400

    onboarding backend started

--- server/routes/employee-router/onboardingApplicationRouter.jsx
+++ server/routes/employee-router/onboardingApplicationRouter.jsx
@@ -1,34 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
 const db = require("../../db_conn/db_conn");
-const userCollection = db.collection("users");
-const jwt = require('jsonwebtoken');
-const {JWT_KEY} = process.env;
+const UserInfo = require ("../../model/UserInfo");
+const userInfoCollection = db.collection("userInfos");
 
 router.post('/onboarding-application', async function (req, resp) {
   let info = req.body;
-  user.username = user.username.toLowerCase();
-  // userCollection.findOne({username : user.username})
-  // .then(function (foundUser) {
-  //   if (foundUser != undefined) {
-  //     bcrypt.compare(user.password, foundUser.password, function (err, res) {
-  //       if (res) {
-  //         resp.set('jwt', jwt.sign(user, JWT_KEY));
-  //         resp.status(201).send('Login Successful!');
-  //       }
-  //       else {
-  //         resp.status(200).send('Wrong Password');
-  //       }
-  //     });
-  //   }
-  //   else {
-  //     resp.status(200).send('No User Information Found');
-  //   }
-  // })
-  // .catch((e) => {
-  //   resp.status(400).send(e);
-  // });
+  try {
+    await UserInfo.create(req.body);
+    resp.status(201).send('Your application is sent to HR.');
+  }
+  catch (e) {
+    resp.status(400).send(e.message);
+  }
+
 });
 
 module.exports = router;
