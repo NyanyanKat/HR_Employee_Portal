19a10fbfd327f20f3ec8667db7b708b27c00899b
Danling <danlingsun@gmail.com>
2022-06-27 20:16:13 -0400

    Registration Input Validation
    Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import Navigation from "./components/Navigation/Navigation";
 import "./App.css";
-import Registration from './components/Authentication/Registration';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import Registration from './components/Authentication/Registration';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 
--- server/routes/registerRouter.js
+++ server/routes/registerRouter.js
@@ -1,57 +1,59 @@
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
-const User = require ("../model/User");
+const User = require("../model/User");
 const jwt = require('jsonwebtoken')
+const { body, validationResult } = require("express-validator");
 
-
-const {body, validationResult} = require("express-validator");
 
 // Danling - Registration Token
-router.get('/register', async(req,resp)=>{
+router.get('/register', async (req, resp) => {
     try {
         const hr_token = await jwt.sign({}, process.env.JWT_KEY)
-        resp.status(200).send({token:hr_token})        
+        resp.status(200).send({ token: hr_token })
     } catch (error) {
-        resp.status(500).send("Failed to generate token",error)
+        resp.status(500).send("Failed to generate token", error)
     }
 })
 
 // Jimmy - Registration 
 router.post("/register", body('email').isEmail(), async (req, resp) => {
+    // console.log(req.body)
     const errors = validationResult(req);
 
-   // username and email should be case insensitive when checking
-   req.body.username = req.body.username.toLowerCase();
-   req.body.email = req.body.email.toLowerCase();
+    // username and email should be case insensitive when checking
+    req.body.username = req.body.username.toLowerCase();
+    req.body.email = req.body.email.toLowerCase();
+    // username and email should be case insensitive when checking
+    req.body.username = req.body.username.toLowerCase();
+    req.body.email = req.body.email.toLowerCase();
 
+    let errMsg = {}
+    const { username, email, password } = req.body;
+    const checkuser = await User.findOne({ username })
+    const checkemail = await User.findOne({ email })
+    if (checkemail) {
+        errMsg.email = 'the email already exists'
+        return resp.status(400).send(JSON.stringify(errMsg))
+    }
+    if (!username) {
+        errMsg.username = 'username can not be empty.'
+    }
+    if (!password) {
+        errMsg.password = 'password can not be empty'
+    }
+    if (checkuser) {
+        errMsg.username = 'the username already exists'
+    }
     try {
-        const {username, email, password} = req.body;
-        const checkuser = await User.findOne({username}) 
-        const checkemail = await User.findOne({email}) 
-        if (!username || !password || !email) {
-            resp.send("Missing one field");
-        }  
-        else if (!errors.isEmpty() && errors.errors[0].param === 'email') {
-            resp.send('Invalid email format. Please try again.');
-        }
-        else if (checkuser || checkemail) {
-            resp.send("User already exists");
-        }
-        else {
-            await User.create(req.body);
-            resp.status(201).send('Successfully registered. Please log in.');
-        }
-    } catch(e) {
-        console.log(e);
-        resp.status(400).send(e);
+        if (Object.keys(errMsg).length != 0)
+            throw new Error(JSON.stringify(errMsg))
+        await User.create(req.body);
+        resp.status(201).send('Successfully registered. Please log in.');
+    }
+    catch (e) {
+        // console.log(e)
+        resp.status(400).send(e.message);
     }
 });
 
-
-// router.get("/delete", async (req, resp) => {
-//     await User.deleteMany();
-//     resp.send("Database deleted");
-// })
-
-
-module.exports = router;
+module.exports = router
