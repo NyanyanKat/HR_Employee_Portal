94bca4ca329eabd6f8e24a1e92d036a25e452e5d
Winston Gao <lgao9@ucsc.edu>
2022-06-30 12:15:26 -0400

    manage server file location: housing_emp

--- server/routes/employee-router/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const housingRoutes = require('./housingRouter');
-const onboardingRoutes = require('./onboardingApplicationRouter');
-
-router.use('/housing', housingRoutes);
-router.use('/onboarding', onboardingRoutes);
-
-module.exports = router;
